e8ed82bbb53a7826d90106513c1b5566c247078c
Shaayaan Sayed <ssayed@cs.utexas.edu>
2016-03-30 10:11:59 -0500

    feat expander supports 3d tensors

--- misc/net_utils.lua
+++ misc/net_utils.lua
@@ -79,31 +79,42 @@ end
 
 -- layer that expands features out so we can forward multiple sentences per image
 local layer, parent = torch.class('nn.FeatExpander', 'nn.Module')
-function layer:__init(n)
+function layer:__init(n, dim)
   parent.__init(self)
   self.n = n
+  self.dim = dim
 end
 function layer:updateOutput(input)
   if self.n == 1 then self.output = input; return self.output end -- act as a noop for efficiency
   -- simply expands out the features. Performs a copy information
-  assert(input:nDimension() == 2)
-  local d = input:size(2)
-  self.output:resize(input:size(1)*self.n, d)
-  for k=1,input:size(1) do
-    local j = (k-1)*self.n+1
-    self.output[{ {j,j+self.n-1} }] = input[{ {k,k}, {} }]:expand(self.n, d) -- copy over
+  if self.dim == 2 then
+    assert(input:nDimension() == 2)
+    local d = input:size(2)
+    self.output:resize(input:size(1)*self.n, d)
+    for k=1,input:size(1) do
+      local j = (k-1)*self.n+1
+      self.output[{ {j,j+self.n-1} }] = input[{ {k,k}, {} }]:expand(self.n, d) -- copy over
+    end
+  else
+    assert(input:nDimension() == 3)
+    self.output:resize(input:size(1)*self.n, input:size(2), input:size(3))
+    for k=1,input:size(1) do
+      local j = (k-1)*self.n+1
+      self.output[{ {j,j+self.n-1} }] = input[{ {k,k}, {}, {} }]:expand(self.n, input:size(2), input:size(3)) -- copy over
+    end
   end
+
   return self.output
 end
 function layer:updateGradInput(input, gradOutput)
   if self.n == 1 then self.gradInput = gradOutput; return self.gradInput end -- act as noop for efficiency
   -- add up the gradients for each block of expanded features
   self.gradInput:resizeAs(input)
-  local d = input:size(2)
   for k=1,input:size(1) do
     local j = (k-1)*self.n+1
     self.gradInput[k] = torch.sum(gradOutput[{ {j,j+self.n-1} }], 1)
   end
+
   return self.gradInput
 end
 
