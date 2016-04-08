0a1f7929e037b0fd13fcd7b00a88cf69f8ab15e1
Shaayaan Sayed <ssayed@cs.utexas.edu>
2016-04-08 18:13:22 -0500

    added clone_list

--- misc/utils.lua
+++ misc/utils.lua
@@ -68,4 +68,15 @@ function utils.average_values(t)
   return vsum / n
 end
 
+function utils.clone_list(tensor_list, zero_too)
+    -- takes a list of tensors and returns a list of cloned tensors
+    local out = {}
+    for k,v in pairs(tensor_list) do
+        out[k] = v:clone()
+        if zero_too then out[k]:zero() end
+    end
+    return out
+end
+
 return utils
+
